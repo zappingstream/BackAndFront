c2f617a2409bf8d67735af750d0b006bea8361bb
l1salvatore <lucas_salva_15@hotmail.com>
2026-05-26 08:26:38 -0300

    Fixes

--- ZappingStreamingChannelsResync/ZappingStreamingChannelsResync/ZappingStreamingChannelsResync.cs
+++ ZappingStreamingChannelsResync/ZappingStreamingChannelsResync/ZappingStreamingChannelsResync.cs
@@ -125,7 +125,7 @@ namespace ZappingStreamingDBService
                 _logger.LogInformation("=== INICIANDO TAREAS DE MANTENIMIENTO ===");
 
                 await ProcesarYActualizarCanalesAsync(stoppingToken);
-             //   await RenovarSuscripcionesWebhooksAsync(stoppingToken);
+                await RenovarSuscripcionesWebhooksAsync(stoppingToken);
 
                 _logger.LogInformation("=== TODAS LAS TAREAS COMPLETADAS CON ÉXITO ===");
             }
